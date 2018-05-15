c852598a5d762c9fdff7bea9bd1ff31baf939fd6
Nicolas Hampton <nicolasjhampton@gmail.com>
2018-05-15 16:04:21 -0700

    cleaning up simple_event_pusher.go

--- cmd/simple_event_pusher.go
+++ cmd/simple_event_pusher.go
@@ -21,13 +21,7 @@ func main() {
 	port := env.Default("EVENT_PUSHER_PORT", DEFAULT_PORT)
 	redisAddr := env.Default("EVENT_PUSHER_REDIS_ADDR", DEFAULT_REDIS_ADDR)
 	redisPubsubPrefix := env.Default("DEFAULT_REDIS_PUBSUB_CHANNEL_PREFIX", DEFAULT_REDIS_PUBSUB_CHANNEL_PREFIX)
-	// redisPubsubChannel := env.Default("EVENT_PUSHER_REDIS_PUBSUB_CHANNEL", DEFAULT_REDIS_PUBSUB_CHANNEL)
 	accessControlAllowOrigin := env.Default("EVENT_PUSHER_ACCESS_CONTROL_ALLOW_ORIGIN", DEFAULT_ACCESS_CONTROL_ALLOW_ORIGIN)
-
-	s := &http.Server{
-		Addr:        port,
-		ReadTimeout: 15 * time.Second,
-	}
 
 	// Threadsafe collection of connections
 	connStore := mux.New()
@@ -47,24 +41,26 @@ func main() {
 	// Listen for new connections
 	// https://golang.org/pkg/net/http/#Server.ListenAndServe
 	// https://golang.org/pkg/net/http/#Server.Serve
+	s := &http.Server{
+		Addr:        port,
+		ReadTimeout: 15 * time.Second,
+	}
 	s.ListenAndServe()
 }
 
 // Completely separate handler for load balancer health check.
 // Not significant part of main data flow.
+//
+// From the docs:
+//
+// If WriteHeader has not yet been called, Write calls
+// WriteHeader(http.StatusOK) before writing the data. If the Header
+// does not contain a Content-Type line, Write adds a Content-Type set
+// to the result of passing the initial 512 bytes of written data to
+// DetectContentType.
+// 
+// So this is the only line needed for status 200 ok, text/plain
 func handleHealthCheck(w http.ResponseWriter, r *http.Request) {
-
-	// Write writes the data to the connection as part of an HTTP reply.
-	//
-	// If WriteHeader has not yet been called, Write calls
-	// WriteHeader(http.StatusOK) before writing the data. If the Header
-	// does not contain a Content-Type line, Write adds a Content-Type set
-	// to the result of passing the initial 512 bytes of written data to
-	// DetectContentType.
-
-	// w.WriteHeader(200)
-	// w.Header().Add("Content-Type", "text/plain")
-	fmt.Println("hi there")
 	w.Write([]byte("OK"))
 }
 
